1867fe2a81d0a60a765c1b5251f084c684cf6667
PatsakornW <patsakorn.wtnsb@gmail.com>
2023-09-12 13:31:22 +0700

    Update search.jsx

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -6,7 +6,7 @@ function Search() {
 
     return (
         <input type="text" placeholder='Search...'
-            className='my-10 p-2 rounded-lg border border-slate-200 focus:outline-none w-1/2'
+            className='mb-10 p-2 rounded-lg border border-slate-200 focus:outline-none w-3/4 sm:w-1/2'
             value={search}
             onChange={(e) => setSearch(e.target.value)}
         />
