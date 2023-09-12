a4f45eea1e1b707db8f688b3481d6852c71d37c3
PatsakornW <patsakorn.wtnsb@gmail.com>
2023-09-12 16:28:04 +0700

    Update search.jsx

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -4,7 +4,7 @@ import { WeatherContext } from '../context/weather'
 function Search() {
     const { search, setSearch } = useContext(WeatherContext)
     return (
-        <input type="text" placeholder='Search...'
+        <input type="text" placeholder='Search country or provice ....'
             className='mb-10 p-2 rounded-lg border border-slate-200 focus:outline-none w-3/4 sm:w-1/2'
             value={search}
             onChange={(e) => setSearch(e.target.value)}
