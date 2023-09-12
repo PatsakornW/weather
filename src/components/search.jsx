703cb71cedb7881f159125031284d4036c6904ad
PatsakornW <patsakorn.wtnsb@gmail.com>
2023-09-12 16:38:25 +0700

    fix

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,14 +1,25 @@
 import React, { useContext } from 'react'
 import { WeatherContext } from '../context/weather'
+import pin from '../assets/icons8-pin-32.png'
+
 
 function Search() {
     const { search, setSearch } = useContext(WeatherContext)
     return (
-        <input type="text" placeholder='Search country or provice ....'
-            className='mb-10 p-2 rounded-lg border border-slate-200 focus:outline-none w-3/4 sm:w-1/2'
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-        />
+        <div className='relative'>
+            <input
+                type="text"
+                placeholder='Search country or province ....'
+                className='mb-10 p-2 rounded-lg border border-slate-200 focus:outline-none w-full'
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <img src={pin} alt="pin" className=' absolute right-2 top-2 w-6 h-6 flex items-center' />
+        </div>
+
+
+
+
     )
 }
 
